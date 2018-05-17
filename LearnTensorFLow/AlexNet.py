dcc1793019863b61d417911fac9e8f62108cd93b
gaolei <buaagaolei@163.com>
2018-05-17 22:10:25 +0800

    AlexNet: 完成了正向计算过程，添加了性能评估的方法

--- LearnTensorFLow/AlexNet.py
+++ LearnTensorFLow/AlexNet.py
@@ -7,7 +7,6 @@ import tensorflow as tf
 batch_size = 32
 num_batches = 100
 
-# 显示op的名称和尺寸
 def print_tensor(tensor):
     '''显示op的名称和尺寸
     '''
@@ -18,8 +17,10 @@ def inference(images):
 
     @Param:
         images:输入图片batch
-    @Return:
-        '''
+    @Returns:
+        fc3(tensor):最后一个全连接层的输出
+        parameters(list):参数列表'''
+
     parameters = [] # 参数列表
 
     # 第一个卷积层
@@ -31,8 +32,8 @@ def inference(images):
         conv1 = tf.nn.relu(bias, name=scope)
         print_tensor(conv1)
         parameters += [kernel, biases]
-        lrn1 = tf.nn.lrn(conv1, depth_radius=4, bias=1,alpha=0.001/9,beta=0.75, name='lrn1')
-        max_pool1 = tf.nn.max_pool(lrn1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='max_pool1')
+        #lrn1 = tf.nn.lrn(conv1, depth_radius=4, bias=1,alpha=0.001/9,beta=0.75, name='lrn1')
+        max_pool1 = tf.nn.max_pool(conv1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='max_pool1')
         print_tensor(max_pool1)
 
     # 第二个卷积层
@@ -44,8 +45,8 @@ def inference(images):
         conv2 = tf.nn.relu(bias, name=scope)
         print_tensor(conv2)
         parameters += [kernel, biases]
-        lrn2 = tf.nn.lrn(conv2, depth_radius=4, bias=1,alpha=0.001/9,beta=0.75, name='lrn2')
-        max_pool2 = tf.nn.max_pool(lrn2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='max_pool2')
+        #lrn2 = tf.nn.lrn(conv2, depth_radius=4, bias=1,alpha=0.001/9,beta=0.75, name='lrn2')
+        max_pool2 = tf.nn.max_pool(conv2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='max_pool2')
         print_tensor(max_pool2)
 
     # 第三个卷积层
@@ -79,4 +80,77 @@ def inference(images):
         parameters += [kernel, biases]
         max_pool5 = tf.nn.max_pool(conv5, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID',name='max_pool5')
         print_tensor(max_pool5)
-        return max_pool5, parameters
+    
+    # 第一个全连接层
+    with tf.name_scope('fc1') as scope:
+        max_pool5_reshape = tf.reshape(max_pool5, shape=[batch_size, -1], name='max_pool5_reshape')
+        dim = max_pool5_reshape.get_shape()[1].value
+        weights = tf.Variable(tf.truncated_normal(shape=[dim, 4096], dtype=tf.float32, stddev=0.1), name='weights')
+        biases = tf.Variable(tf.constant(0.1, dtype=tf.float32, shape=[4096]), name='biases')
+        fc1 = tf.nn.relu(tf.nn.bias_add(tf.matmul(max_pool5_reshape, weights), biases), name=scope)
+        print_tensor(fc1)
+        parameters += [weights, biases]
+
+    # 第二个全连接层
+    with tf.name_scope('fc2') as scope:
+        weights = tf.Variable(tf.truncated_normal(shape=[4096, 4096], dtype=tf.float32, stddev=0.1), name='weights')
+        biases = tf.Variable(tf.constant(0.1, dtype=tf.float32, shape=[4096]), name='biases')
+        fc2 = tf.nn.relu(tf.nn.bias_add(tf.matmul(fc1, weights), biases), name=scope)
+        print_tensor(fc2)
+        parameters += [weights, biases]
+
+    # 第三个全连接层
+    with tf.name_scope('fc3') as scope:
+        weights = tf.Variable(tf.truncated_normal(shape=[4096, 1000], dtype=tf.float32, stddev=0.1), name='weights')
+        biases = tf.Variable(tf.constant(0.1, dtype=tf.float32, shape=[1000]), name='biases')
+        fc3 = tf.nn.softmax(tf.nn.bias_add(tf.matmul(fc2, weights), biases), name=scope)
+        print_tensor(fc3)
+        parameters += [weights, biases]
+
+    return fc3, parameters
+    
+def time_tensorflow_run(session, target, info_string):
+    '''评估每轮计算所需时间
+
+    @Params:
+        session: TensorFlow Session
+        target: 要进行评估的运算算子
+        info_string: 测试的名称
+    @Return:
+        None
+    '''
+    num_step_burn_in = 10
+    total_duration = 0.0
+    total_duration_squared = 0.0 # 总时间的平方和，用于计算方差
+    for i in range(num_batches + num_step_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_step_burn_in:
+            if i%10 == 0:
+                print('%s: step %d, duration: %.3f' % (datetime.now(), i-num_step_burn_in, duration))
+            total_duration += duration
+            total_duration_squared += duration*duration
+    
+    mean_duration = total_duration / num_batches
+    stdvar_duration = math.sqrt(total_duration_squared / num_batches - mean_duration * mean_duration)
+    print('%s: %s across %d steps, %.3f +/- %.3f sec/batch' % (datetime.now(), info_string, num_batches, mean_duration, stdvar_duration))
+
+def run_benchmark():
+    '''测试AlexNet的计算耗时
+    '''
+    with tf.Graph().as_default():
+        image_size = 224
+        images = tf.Variable(tf.random_normal(shape=[batch_size, image_size, image_size, 3], dtype=tf.float32, stddev=0.1))
+        fc5, parameters = inference(images)
+
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init)
+        time_tensorflow_run(sess, fc5, 'Forward')
+
+        loss = tf.nn.l2_loss(fc5)
+        grad = tf.gradients(loss, parameters)
+        time_tensorflow_run(sess, grad, 'Backward')
+
+run_benchmark()
